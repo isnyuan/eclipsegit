3e70739c52f146cb07a191948300a2611181f846
isnyuan <67676288+isnyuan@users.noreply.github.com>
2020-07-01 20:47:45 +0800

    Update Test.java

--- gittest/src/gittest/Test.java
+++ gittest/src/gittest/Test.java
@@ -1,9 +1,5 @@
 package gittest;
-<<<<<<< HEAD
-//aaa
-=======
 //lhzhaha
->>>>>>> branch 'master' of https://github.com/isnyuan/eclipsegit.git
 public class Test {
 
 	public static void main(String[] args) {
